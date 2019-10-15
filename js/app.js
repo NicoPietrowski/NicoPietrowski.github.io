aae83a6f06a775b1c29d19c81158f03a94372b3d
Nicolas Pietrowski <n.pietrowski@octipas.com>
2019-10-15 16:01:02 +0200

    photo

--- js/app.js
+++ js/app.js
@@ -101,7 +101,7 @@ function initialize()
 	});
 	var mesh = new THREE.Mesh(geometry, material);
 	mesh.position.y = 0.5;
-	mesh.rotation = -1.5708;
+	mesh.rotation = -90;
 	markerRoot1.add(mesh);
 }
 
